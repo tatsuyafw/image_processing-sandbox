de42f13d99693dde12d9f02ae2e8eec5d3ed725d
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2020-03-05 00:28:06 +0900

    Allow to pass a target pdf as an argument

--- process.rb
+++ process.rb
@@ -1,9 +1,24 @@
 require "image_processing/vips"
 
-file = File.new('sample.pdf')
+path = ARGV[0]
+
+if path.nil?
+  warn "Need a pdf file"
+  exit(1)
+end
+
+unless File.exist?(path)
+  warn "#{path} not found"
+  exit(1)
+end
+
+file = File.new(path)
+
+ext = 'png'
+base = File.basename(path, '.*')
 
 ImageProcessing::Vips
   .source(file)
   .crop(50, 50, 350, 350)
   .convert("png")
-  .call(destination: 'sample.png')
+  .call(destination: "#{base}.#{ext}")
